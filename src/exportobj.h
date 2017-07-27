82d1136713049ccbe6ae33e849014fdf56c825c4
Amir Baserinia <amir@ennova2.ennova-cfd.com>
2017-07-27 14:35:47 -0700

    export to OBJ

---
+++ src/exportobj.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2017 Amir Baserinia
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef TYPE_EXPORTOBJ_H_
+#define TYPE_EXPORTOBJ_H_
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "visitor.h"
+#include "geometry.h"
+
+class ExportOBJ : public Visitor<Geometry> {
+    std::string filename_;
+public:
+    ExportOBJ(const std::string& filename) :
+        filename_(filename) {}
+
+    void dispatch(Geometry& model) override {
+        std::cout << "Saving OBJ file: " << filename_ << std::endl;
+        save();
+    }
+
+    void save() {}
+};
+
+#endif // TYPE_EXPORTOBJ_H_
